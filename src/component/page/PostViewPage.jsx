d374acfb773a7d4e3b9b713f94e1f6d50ca2fafb
tmdrbs0925 <tmdrbs0925@gmail.com>
2024-04-18 00:22:03 +0900

    changed

--- src/component/page/PostViewPage.jsx
+++ src/component/page/PostViewPage.jsx
@@ -53,7 +53,7 @@ function PostViewPage(props){
     const {postId} = useParams();
 
     const post = data.find((item)=>{
-        return item.id === postId;
+        return item.id == postId;
     });
 
     const [comment, setComment] = useState("");
